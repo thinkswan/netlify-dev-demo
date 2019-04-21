15182ef6f3d4b072297f62d72c6bf3c92b749b76
Graham Swan <thinkswan@gmail.com>
2019-04-21 21:17:58 +0200

    Fetch list of users from JSONPlaceholder API

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,4 +5,17 @@ const getWord = async () => {
   document.getElementById("random-word").innerText = word
 }
 
+const getUsers = async () => {
+  const res = await fetch("/api/users")
+  const users = await res.json()
+
+  users.forEach(user => {
+    const li = document.createElement("li")
+    const text = document.createTextNode(user.name)
+    li.appendChild(text)
+    document.getElementById("users").appendChild(li)
+  })
+}
+
 getWord()
+getUsers()
